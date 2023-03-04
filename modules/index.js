94b2ff4f2063777a08a375fcf958271c228d37bd
vinayakkoliwad215 <vinayak@vishumoney.com>
2023-03-04 17:17:09 +0530

    first commit

---
+++ modules/index.js
@@ -0,0 +1,6 @@
+let a ="Vinayak Koliwad Hello World Program";
+
+console.log(a); 
+const add = require("./sub");
+const sum = add(2,3)
+console.log(sum); 
---
+++ modules/sub.js
@@ -0,0 +1,7 @@
+function add(a,b) {
+	return a-b;
+}
+module.exports = add
+// const sum = add(82,3);
+
+// console.log(sum);
