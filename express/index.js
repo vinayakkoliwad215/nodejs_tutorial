8d53912ec68ee998caa3ff1e4d675e1dcd37bc12
vinayakkoliwad215 <vinayak@vishumoney.com>
2023-03-12 11:43:59 +0530

    html and render file

--- express/index.js
+++ express/index.js
@@ -1,14 +1,25 @@
 const express = require("express");
+const path = require("path");
 
 const app= express()
+const pathdir = path.join(__dirname,'public');
+app.use(express.static(pathdir))
+//console.log(pathdir);
 app.get("/",(req,res)=>{
-	res.send("hi this is first express route");
+	res.sendFile(`${pathdir}/index.html`);
+})
+
+app.get("/home",(req,res)=>{
+	res.sendFile(`${pathdir}/home.html`);
 })
 
 app.get("/about",(req,res)=>{
-	res.send("hi this is first about express route");
+	res.send(`<h1>hi this is first about  express route</h1>
+
+		<input type="text" placeholder="name" value="vinayak"/>
+		`);
 })
 
 app.listen(3000,()=>{
-	console.log("server extibish");
+	console.log("server estabilhed");
 })
